1091fa3d3fcfc17e60336de9cf953ddeceeb5a9b
Oscar Bautista <oscar.adba@gmail.com>
2023-03-12 21:29:43 -0500

    📢 :european_castle:  Added marketing deployment

--- shell/config/webpack.prod.js
+++ shell/config/webpack.prod.js
@@ -4,17 +4,18 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const domain = process.env.PRODUCTION_DOMAIN;
+
 const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/shell/latest',
+    publicPath: '/shell/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
