8cbfc68eb0987c7d1f1af0f0d414e74de48322dd
Oscar Bautista <oscar.adba@gmail.com>
2023-03-18 21:35:30 -0500

    📢 Communicating Through Callbacks

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory } from 'history';
 import App from './App';
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, el);
 };
 
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#marketing-root');
-  if (devRoot) mount(devRoot);
+  if (devRoot) mount(devRoot, {});
 }
 
 export { mount };
--- shell/src/components/MarketingApp.js
+++ shell/src/components/MarketingApp.js
@@ -1,11 +1,20 @@
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { mount } from 'marketing/MarketingApp';
 
 const MarketingApp = () => {
   const marketingRef = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(marketingRef.current);
+    mount(marketingRef.current, {
+      onNavigate: ({ pathname: nextPathName }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathName) {
+          history.push(nextPathName);
+        }
+      },
+    });
   }, []);
 
   return <div ref={marketingRef}></div>;
